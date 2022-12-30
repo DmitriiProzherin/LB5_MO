3e907c6f057b8f7f84c95ff379535afea63950a2
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-31 01:44:05 +0300

    updated output

--- src/GoldenRatio.java
+++ src/GoldenRatio.java
@@ -1,14 +1,18 @@
 public class GoldenRatio {
     private static int k = 0;
     public static final double phi = (Math.pow(5, 1/2.0) - 1) / 2.0;
-
     public static double f(double x){
         return Math.pow(x, 2);
     }
 
-
     public static double search_naive_recursive(double a, double b, double eps){
         double length = b - a;
+
+        if (length < 2*eps) {
+            k = 0;
+            return a + length / 2.0;
+        }
+
         double left = a + length * (1 - phi);
         double right = a + length * phi;
 
@@ -18,10 +22,6 @@ public class GoldenRatio {
         System.out.printf("%3d%10f%10f%10f%10f%10f%10f%10f\n",
                 k, length, a, b, left, right, f_left, f_right);
         k++;
-        if (length < 2*eps) {
-            k = 0;
-            return a + length / 2.0;
-        }
 
         if (f_left < f_right) return search_naive_recursive(a, right, eps);
         else return search_naive_recursive(left, b, eps);
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
-
+    static double eps = 1e-4 / 2.0;
     public static void main(String[] args) {
     System.out.printf("%3s%10s%10s%10s%10s%10s%10s%10s\n",
                 "k", "length", "a", "b", "left", "right", "f_left", "f_right");
-    double x_min = GoldenRatio.search_naive_recursive(-1, 1, 0.05);
+    double x_min = GoldenRatio.search_naive_recursive(-1, 1, eps);
     double f_min = GoldenRatio.f(x_min);
     System.out.printf("Точка минимума: %10f\nМинимальное значение функции: %10f", x_min, f_min);
-
     }
 }
